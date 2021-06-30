d6c736b27b98da4fdb5f34cccdcbdd5e33b60bdf
anganevagg <anganevagg@gmail.com>
2021-06-30 12:04:11 -0500

    modificado

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from src import *
 
-
-
 if __name__ == "__main__":
     print("Main inicializado")
     g = Game.Game()
---
+++ src/Sprite.py
@@ -0,0 +1,9 @@
+import pygame
+
+
+import pygame
+
+class Sprite:
+    def __init__(self, src):
+        pygame.image.load(src).conver()
+
