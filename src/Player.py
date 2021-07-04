9565f2e294b2e652c9ca409eeda78c4817442f59
anganevagg <anganevagg@gmail.com>
2021-07-04 08:41:26 -0500

    nothing

--- src/Player.py
+++ src/Player.py
@@ -4,28 +4,28 @@ import pygame
 
 
 
-reposo=[pygame.image.load("./src/media/movimientosJugador/reposoDerecha.png"),
-       pygame.image.load("./src/media/movimientosJugador/reposoIzquierda.png")]
+# reposo=[pygame.image.load("./src/media/movimientosJugador/reposoDerecha.png"),
+#        pygame.image.load("./src/media/movimientosJugador/reposoIzquierda.png")]
 
-caminarDerecha=[pygame.image.load("./src/media/movimientosJugador/corriendoDerecha1.png"),
-               pygame.image.load("./src/media/movimientosJugador/corriendoDerecha2.png"),
-               pygame.image.load("./src/media/movimientosJugador/corriendoDerecha3.png"),
-               pygame.image.load("./src/media/movimientosJugador/corriendoDerecha4.png")]
+# caminarDerecha=[pygame.image.load("./src/media/movimientosJugador/corriendoDerecha1.png"),
+#                pygame.image.load("./src/media/movimientosJugador/corriendoDerecha2.png"),
+#                pygame.image.load("./src/media/movimientosJugador/corriendoDerecha3.png"),
+#                pygame.image.load("./src/media/movimientosJugador/corriendoDerecha4.png")]
 
-caminaIzquierda=[pygame.image.load("./src/media/movimientosJugador/corriendoIzuierda1.png"),
-                 pygame.image.load("./src/media/movimientosJugador/corriendoIzquierda2.png"),
-                 pygame.image.load("./src/media/movimientosJugador/corriendoIzquierda3.png"),
-                 pygame.image.load("./src/media/movimientosJugador/corriendoIzquierda4.png")]
+# caminaIzquierda=[pygame.image.load("./src/media/movimientosJugador/corriendoIzuierda1.png"),
+#                  pygame.image.load("./src/media/movimientosJugador/corriendoIzquierda2.png"),
+#                  pygame.image.load("./src/media/movimientosJugador/corriendoIzquierda3.png"),
+#                  pygame.image.load("./src/media/movimientosJugador/corriendoIzquierda4.png")]
 
-saltoDerecha=[pygame.image.load("./src/media/movimientosJugador/saltoDerecha1.png"),
-             pygame.image.load("./src/media/movimientosJugador/saltoDerecha2.png"),
-             pygame.image.load("./src/media/movimientosJugador/saltoDerecha3.png"),
-             pygame.image.load("./src/media/movimientosJugador/saltoDerecha4.png")]
+# saltoDerecha=[pygame.image.load("./src/media/movimientosJugador/saltoDerecha1.png"),
+#              pygame.image.load("./src/media/movimientosJugador/saltoDerecha2.png"),
+#              pygame.image.load("./src/media/movimientosJugador/saltoDerecha3.png"),
+#              pygame.image.load("./src/media/movimientosJugador/saltoDerecha4.png")]
 
-saltoIzquierda=[pygame.image.load("./src/media/movimientosJugador/saltoIzquierda1.png"),
-               pygame.image.load("./src/media/movimientosJugador/saltoIzquierda2.png"),
-               pygame.image.load("./src/media/movimientosJugador/saltoIzquierda3.png"),
-               pygame.image.load("./src/media/movimientosJugador/saltoIzquierda4.png")]
+# saltoIzquierda=[pygame.image.load("./src/media/movimientosJugador/saltoIzquierda1.png"),
+#                pygame.image.load("./src/media/movimientosJugador/saltoIzquierda2.png"),
+#                pygame.image.load("./src/media/movimientosJugador/saltoIzquierda3.png"),
+#                pygame.image.load("./src/media/movimientosJugador/saltoIzquierda4.png")]
 
 class Player(Sprite):
     def __init__(self):
